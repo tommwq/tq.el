e683fe2ccf8097f7bd0e47a55a9a62a8485a2b83
汪千 <wangqian2@guosen.com.cn>
2024-06-05 14:03:34 +0800

    feat

--- tq-settings.el
+++ tq-settings.el
@@ -175,16 +175,49 @@
 
 
 "设置字符编码。"
-(setf file-name-coding-system 'utf-8
-      default-file-name-coding-system 'utf-8-unix
-      default-buffer-file-coding-system 'utf-8-unix)
-(set-language-environment "UTF-8")
-;; 设置文件缓冲区默认保存编码。
-(set-buffer-file-coding-system 'utf-8-unix)
-(prefer-coding-system 'chinese-gbk-dos)
-(prefer-coding-system 'chinese-gbk-unix)
+;; (setf file-name-coding-system 'utf-8
+;;       default-file-name-coding-system 'utf-8-unix
+;;       default-buffer-file-coding-system 'utf-8-unix)
+
+
+(set-language-environment 'Chinese-GBK)
+
+
+;; 编码设置 begin
+(set-language-environment 'Chinese-GB)
+;; default-buffer-file-coding-system变量在emacs23.2之后已被废弃，使用buffer-file-coding-system代替
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(setq-default pathname-coding-system 'euc-cn)
+(setq file-name-coding-system 'euc-cn)
+;; 另外建议按下面的先后顺序来设置中文编码识别方式。
+;; 重要提示:写在最后一行的，实际上最优先使用; 最前面一行，反而放到最后才识别。
+;; utf-16le-with-signature 相当于 Windows 下的 Unicode 编码，这里也可写成
+;; utf-16 (utf-16 实际上还细分为 utf-16le, utf-16be, utf-16le-with-signature等多种)
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+;;(prefer-coding-system 'gb18030)
+;(prefer-coding-system 'utf-16le-with-signature)
+(prefer-coding-system 'utf-16)
+;; 新建文件使用utf-8-unix方式
+;; 如果不写下面两句，只写
+;; (prefer-coding-system 'utf-8)
+;; 这一句的话，新建文件以utf-8编码，行末结束符平台相关
 (prefer-coding-system 'utf-8-dos)
 (prefer-coding-system 'utf-8-unix)
+;; 编码设置 end
+
+
+
+
+;; 设置文件缓冲区默认保存编码。
+
+
+;; (set-buffer-file-coding-system 'utf-8-unix)
+;; (prefer-coding-system 'chinese-gbk-dos)
+;; (prefer-coding-system 'chinese-gbk-unix)
+;; (prefer-coding-system 'utf-8-dos)
+;; (prefer-coding-system 'utf-8-unix)
 
 (tq-util-maximize-window)
-
